922ea76d819b63f6f0a7b34ea5a6be1ef4720777
Hansa2000 <58215839+Hansa2000@users.noreply.github.com>
2023-09-20 14:47:17 +0530

    tested_upto_mem_read_write

--- Datapath/datapath_tb.sv
+++ Datapath/datapath_tb.sv
@@ -77,15 +77,63 @@ initial begin
 	reg1_addr = 5'b00000;
 	reg2_addr = 5'b10011;
 	write_reg_addr = 5'b0;
-	instruction = 32'b 00000001001000000010000100100011 ;// save reg 18 value to mem location 01  , 0000000 10010 00000 010 00010 0100011 
+	instruction = 32'b 00000001001000000010000100100011 ;// save reg 19 value to mem location 02  , 0000000 10010 00000 010 00010 0100011 
 	ALUOp = 3'b000;
 	
 	ctrl0 = 0;
-	ctrl1 = 1;
+	ctrl1 = 0;
 	ctrl2 = 1;
 	ctrl3 = 0;
 	#1 ctrl4 = 1;
 	#9;
+	ctrl4 = 0;
+	#10;
+
+	reg1_addr = 5'b00000;
+	reg2_addr = 5'b00000;
+	write_reg_addr = 5'b11100;
+	instruction = 32'b 00000000000100000010111000000011  ;// load mem loacation 1 value to reg 28  , 0000000 00001 00000 010 11100 0000011 
+	ALUOp = 3'b000;
+	
+	ctrl0 = 1;
+	ctrl1 = 0;
+	ctrl2 = 1;
+	ctrl4 = 0;
+	ctrl3 = 1;
+	#10;
+	ctrl0 = 0;
+	#10;
+	ctrl3 = 0;
+	#10;
+
+	reg1_addr = 5'b00000;
+	reg2_addr = 5'b00000;
+	write_reg_addr = 5'b11101;
+	instruction = 32'b 00000000001000000010111010000011  ;// load mem loacation 2 value to reg 29  , 0000000 00010 00000 010 11101 0000011 
+	ALUOp = 3'b000;
+	
+	ctrl0 = 1;
+	ctrl1 = 0;
+	ctrl2 = 1;
+	ctrl4 = 0;
+	ctrl3 = 1;
+	#10;
+	ctrl0 = 0;
+	#10;
+	ctrl3 = 0;
+
+	instruction = 32'b 00000001110111100000111100110011 ;// add reg 28 and 29,save it to reg 30  , 0000000 11101 11100 000 11110 0110011 
+	reg1_addr = 5'b11100;
+	reg2_addr = 5'b11101;
+	write_reg_addr = 5'b11110;
+	ALUOp = 3'b000;
+	ctrl0 = 1;
+	ctrl1 = 1;
+	ctrl2 = 0;
+	ctrl4 = 0;
+	ctrl3 = 0;
+	#10;
+	ctrl0 = 0;
 	#10;
 	
 end
